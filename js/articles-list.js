04bc0426d6b77d857efa0a246c99604b0ed743ea
edan7895 <motiontvmarketing@gmail.com>
2025-10-17 12:59:58 +0800

    Add files via upload

--- js/articles-list.js
+++ js/articles-list.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
 <div class="cta-section">
   <h2>Latest Articles:</h2>
   <div class="article-grid">
-    <a href="#">Updating</a>
+    <a href="https://www.bookofanswersonline.com/articles/the-power-of-daily-inspiration.html">The Power of Daily Inspiration</a>
     <a href="https://www.bookofanswersonline.com/articles/the-power-of-stillness.html">The Power of Stillness</a>
     <a href="https://www.bookofanswersonline.com/articles/the-magic-of-moon-phases.html">The Magic of Moon Phases</a>
     <a href="https://www.bookofanswersonline.com/articles/the-hidden-strengths-of-each-zodiac-sign.html">The Hidden Strengths of Each Zodiac Sign</a>
